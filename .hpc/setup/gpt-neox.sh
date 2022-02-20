e22c449a5c48eb46eee2fae727d811442726d46c
André Storhaug <andr3.storhaug@gmail.com>
2022-02-20 18:20:41 +0100

    Add HPC instructions

---
+++ .hpc/setup/gpt-neox.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+module load foss/2020b
+module load Anaconda3/2020.07
+module load CUDA/11.3.1
+
+export CXX=g++
+
+python -m venv .venv
+source .venv/bin/activate
+python -m pip install --upgrade pip
+
+#pip install torch
+pip install torch==1.10.2+cu113 -f https://download.pytorch.org/whl/cu113/torch_stable.html
+pip install -r requirements/requirements.txt
+
+python ./megatron/fused_kernels/setup.py install
